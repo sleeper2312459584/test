712448ddf93ccf948da0cc26dc94398b596550fd
sleeper2312459584 <Autry@idealalter.com>
2025-06-23 01:22:37 -0500

    Update docusaurus.config.js
    changed h1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'IDEALalter',
+  tagline: 'We Know All Things Google',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
